95588ced838b9650d2501d79ca0e71f6d8b9c514
fabien moreno <fabienmoreno1331@yahoo.com>
2023-04-05 00:39:22 -0500

    artwork and cat models

---
+++ server/models/Artwork.js
@@ -0,0 +1,58 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const artworkSchema = new Schema({
+	title: {
+		type: String,
+		required: true,
+		unique: false,
+		trim: true,
+	},
+	artist: {
+		type: String,
+		required: true,
+		unique: false,
+		trim: true,
+	},
+	created: {
+		type: Date,
+		default: Date.now,
+		set: (createdAtVal) => new Date(createdAtVal),
+		get: (createdAtVal) =>
+			new Date(createdAtVal).toLocaleDateString("en-US"),
+	},
+	price: {
+		type: Number,
+		required: true,
+		unique: false,
+		trim: true,
+	},
+	quantity: {
+		type: Number,
+		required: true,
+		unique: false,
+		trim: true,
+	},
+	storage_id: {
+		type: String,
+		required: true,
+		unique: false,
+		trim: true,
+	},
+    lore: {
+        type: Text,
+        required: false,
+    },
+	categories: [
+		{
+			type: String,
+			required: true,
+			unique: false,
+			trim: true,
+		},
+	],
+});
+
+const Artwork = model("Artwork", artworkSchema);
+
+module.exports = Artwork;
