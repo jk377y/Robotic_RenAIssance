44150c2a1558a929fc5734767c54a0153b40fe16
rmob <riley.obryan@gmail.com>
2023-04-06 17:29:02 -0700

    forgot to add Artist to index.js module.exports

--- server/models/index.js
+++ server/models/index.js
@@ -3,5 +3,5 @@ const Categories = require('./Categories');
 const Artwork = require('./Artwork'); 
 const Artist = require('./Artist')
 
-module.exports = { User, Categories, Artwork };
+module.exports = { User, Categories, Artwork, Artist };
 
