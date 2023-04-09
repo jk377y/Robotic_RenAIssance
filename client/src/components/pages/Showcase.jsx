0c19f662ee5a371892987146b367447243618ca4
fabien moreno <fabienmoreno1331@yahoo.com>
2023-04-09 18:28:26 -0500

    artworks query destructure

--- client/src/components/pages/Showcase.jsx
+++ client/src/components/pages/Showcase.jsx
@@ -11,3 +11,31 @@ const client = new ApolloClient({
 	uri: "http://localhost:3001/graphql",
 	cache: new InMemoryCache(),
 });
+
+const ARTWORKS_QUERY = gql`
+	query {
+		users {
+			_id
+			username
+			email
+			password
+		}
+		artworks {
+			_id
+			id
+			artist {
+				name
+				story
+				image
+				age
+			}
+			categories
+			created
+			image_id
+			lore
+			price
+			quantity
+			title
+		}
+	}
+`;
