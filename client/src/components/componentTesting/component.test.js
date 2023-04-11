5146558e85ae15dd8d8a08ba9ae4f3e1e26349a5
James Kelly <jk377y@gmail.com>
2023-04-11 13:23:11 -0500

    created showcase jest test

--- client/src/components/componentTesting/component.test.js
+++ client/src/components/componentTesting/component.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from "@testing-library/react";
 // import { App } from "../../App";
-import { Homepage } from "../pages/Homepage";
-
+// import { Homepage } from "../pages/Homepage";
+import Showcase from "../pages/Showcase";
 
 
 
@@ -14,13 +14,17 @@ import { Homepage } from "../pages/Homepage";
 // 	});
 // });
 
-describe("Homepage component", () => {
+// describe("Homepage component", () => {
+// 	test("renders without crashing", () => {
+// 		render(<Homepage />);
+// 	});
+// });
+
+describe("Showcase component", () => {
 	test("renders without crashing", () => {
-		render(<Homepage />);
+		render(<Showcase />);
 	});
 });
-
-
 
 
 
