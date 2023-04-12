a1d72ec9666134d64529794438fdedd79da90d68
Sofia92 <sofiasosaperez@outlook.com>
2023-04-11 21:56:08 -0500

    fixed navbar components

--- client/src/components/helpercomponent/NavBar.jsx
+++ client/src/components/helpercomponent/NavBar.jsx
@@ -2,18 +2,22 @@ import { NavBarWrapper, NavIcon, NavItems } from "../styles/helpers/Navbar.style
 import { VscGear } from "react-icons/vsc";
 import ToolTip from '@mui/material/Tooltip';
 
-
 export const NavBar = () => {
     return <>
-    <NavBarWrapper>
-        <NavItems>
-        <ToolTip title={<h2>Click to Login or Register</h2>} placement="left" arrow>
-        <a href="/login" target="_blank"><VscGear style={{ stroke: "url(#blue-gradient)" }}/></a> 
-        </ToolTip> 
-        <a href="/">Can I get a Hoiyyyaaaaaaaah</a> 
-        <a href="/showcase">Showcase</a>
-        </NavItems>
-    </NavBarWrapper>   
-    </>
+        <NavBarWrapper>
+            <div style={{ display: "flex", alignItems: "center", marginRight: "auto" }}>
+                <a href="/" style={{ marginRight: "20px", fontFamily: 'Orbitron' }}>Home</a>
+                <a href="/showcase" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>Showcase</a>
+                <a href="/us" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>About us</a>
+            </div>
 
+            <NavItems>
+                <ToolTip title={<h2 style={{ fontFamily: 'Orbitron' }}>Click to Login or Register</h2>} placement="left" arrow>
+                    <a href="/login"><VscGear style={{ stroke: "url(#blue-gradient)" }} /></a>
+                </ToolTip>
+            </NavItems>
+        </NavBarWrapper>
+    </>
 }
+
+
