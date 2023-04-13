9427c3371df866cce146b1764c9a0c5fc1a08f75
James Kelly <jk377y@gmail.com>
2023-04-13 06:04:57 -0500

    added structure for delete section on Admin

--- client/src/components/pages/Admin.jsx
+++ client/src/components/pages/Admin.jsx
@@ -51,71 +51,85 @@ const DELETE_USER = gql`
 `;
 
 const AdminData = () => {
-    const [selectedUserId, setSelectedUserId] = useState("");
-    const [newEmail, setNewEmail] = useState("");
-    const { loading, error, data } = useQuery(GET_USERS);
-    const { loading:loadingUserData, error:errorUserData, data:userData } = useQuery(GET_USER, {
-      variables: { id: selectedUserId },
-      skip: !selectedUserId,
-    });
+  const [selectedUserId, setSelectedUserId] = useState("");
+  const [newEmail, setNewEmail] = useState("");
+  const { loading, error, data } = useQuery(GET_USERS);
+  const { loading:loadingUserData, error:errorUserData, data:userData } = useQuery(GET_USER, {
+    variables: { id: selectedUserId },
+    skip: !selectedUserId,
+  });
+  const [updateUser] = useMutation(UPDATE_USER, {
+    refetchQueries: [{ query: GET_USER, variables: { id: selectedUserId } }],
+  });
   
-    const [updateUser] = useMutation(UPDATE_USER, {
-      refetchQueries: [{ query: GET_USER, variables: { id: selectedUserId } }],
-    });
 
-    if (loading) return <p>Loading Data For Users...</p>;
-    if (error) return <p>Error collecting data for Users :</p>;
-    if (loadingUserData) return <p>Loading Data For User...</p>;
-    if (errorUserData) return <p>Error collecting User data</p>;
-  
-    const handleUserDataButtonClick = async (id) => {
-      setSelectedUserId(id);
-    };
-  
-    const handleEmailChange = (event) => {
-      setNewEmail(event.target.value);
-    };
-  
-    const handleEmailSubmit = async (event) => {
-      event.preventDefault();
-  
-      if (!newEmail) {
-        alert('Please enter a new email address');
-        return;
-      }
-  
-      try {
-        await updateUser({ variables: { _id: selectedUserId, email: newEmail } });
-        alert('Email updated successfully');
-        setNewEmail('');
-      } catch (error) {
-        console.error(error);
-        alert('Error updating email');
-      }
-    };
+  const [showModal, setShowModal] = useState(false);
 
-    return (
-      <>
-        <h1>Admin Data</h1>
-        <h2>Users:</h2>
-        <ul>
-          {data.users.map((user) => (
-            <li key={user._id}>
-              {user.username} :  <button onClick={() => handleUserDataButtonClick(user._id)}>Get Data for {user.username}</button>
-            </li>
-          ))}
-        </ul>
-        {userData && (
-            <div>
-            <div>
-              <h2>Selected User Data:</h2>
-              <p>ID: {userData.user._id}</p>
-              <p>Username: {userData.user.username}</p>
-              <p>Email: {userData.user.email}</p>
-              <p>Password: {userData.user.password}</p>
-            </div>
-            <div>
-              <h2>Update Email:</h2>
+  if (loading) return <p>Loading Data For Users...</p>;
+  if (error) return <p>Error collecting data for Users :</p>;
+  if (loadingUserData) return <p>Loading Data For User...</p>;
+  if (errorUserData) return <p>Error collecting User data</p>;
+
+  const handleUserDataButtonClick = async (id) => {
+    setSelectedUserId(id);
+  };
+
+  const handleEmailChange = (event) => {
+    setNewEmail(event.target.value);
+  };
+
+  const handleEmailSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!newEmail) {
+      alert('Please enter a new email address');
+      return;
+    }
+
+    try {
+      await updateUser({ variables: { _id: selectedUserId, email: newEmail } });
+      alert('Email updated successfully');
+      setNewEmail('');
+    } catch (error) {
+      console.error(error);
+      alert('Error updating email');
+    }
+  };
+
+  const handleModalOpen = () => {
+    setShowModal(true);
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
+  const handleDeleteUser = async () => {
+    
+  };
+
+  return (
+    <>
+      <h1>Admin Data</h1>
+      <h2>Users:</h2>
+      <ul>
+        {data.users.map((user) => (
+          <li key={user._id}>
+            {user.username} :  <button onClick={() => handleUserDataButtonClick(user._id)}>Get Data for {user.username}</button>
+          </li>
+        ))}
+      </ul>
+      {userData && (
+        <div>
+          <div>
+            <h2>Selected User Data:</h2>
+            <p>ID: {userData.user._id}</p>
+            <p>Username: {userData.user.username}</p>
+            <p>Email: {userData.user.email}</p>
+            <p>Password: {userData.user.password}</p>
+          </div>
+          <div>
+            <h2>Update Email:</h2>
             <form onSubmit={handleEmailSubmit}>
               <label htmlFor="newEmail">New Email:</label>
               <input
@@ -126,16 +140,29 @@ const AdminData = () => {
               />
               <button type="submit">Update Email</button>
             </form>
+            <button onClick={handleModalOpen}>Delete User</button>
+          </div>
+          {showModal && (
+            <div className="modal">
+              <div className="modal-content">
+                <h2>Confirm Delete User</h2>
+                <p>This action is irreversible and the user will be permanently removed from the system. Do you wish to continue?</p>
+                <div className="modal-buttons">
+                  <button onClick={handleDeleteUser}>Yes</button>
+                  <button onClick={handleModalClose}>No</button>
+                </div>
+              </div>
             </div>
-            </div>
-        )}
-        {loading && <p>Loading Data For Users...</p>}
+          )}
+        </div>
+      )}
+      {loading && <p>Loading Data For Users...</p>}
       {error && <p>Error collecting data for Users :</p>}
       {loadingUserData && <p>Loading Data For User...</p>}
       {errorUserData && <p>Error collecting User data</p>}
-      </>
-    );
-  };
+    </>
+  );
+};
 
 const Admin = () => {
   return (
