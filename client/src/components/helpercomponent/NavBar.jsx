2d55d0d70c70b7d5f8e8dbfaf52b2b8f0887d747
rmob <riley.obryan@gmail.com>
2023-04-13 17:29:37 -0700

    earlier attempt to push this change, logout button to navbar, didn't actually happen.  trying again

--- client/src/components/helpercomponent/NavBar.jsx
+++ client/src/components/helpercomponent/NavBar.jsx
@@ -2,10 +2,14 @@ import { NavBarWrapper, NavIcon, NavItems } from "../styles/helpers/Navbar.style
 import { VscGear } from "react-icons/vsc";
 import ToolTip from '@mui/material/Tooltip';
 import { Link } from "react-router-dom";
+import Auth from '../../utils/auth'
+import { ButtonLogout } from "./ButtonLogout";
 
 
 export const NavBar = () => {
-    return <>
+    if (!Auth.loggedIn()) {
+    return (
+    <>
         <NavBarWrapper>
             <div style={{ display: "flex", alignItems: "center", marginRight: "auto" }}>
                 <Link to="/" style={{ marginRight: "20px", fontFamily: 'Orbitron' }}>Home</Link>
@@ -20,4 +24,24 @@ export const NavBar = () => {
             </NavItems>
         </NavBarWrapper>
     </>
+    )} else {
+        return (
+            <>
+                <NavBarWrapper>
+                    <div style={{ display: "flex", alignItems: "center", marginRight: "auto" }}>
+                        <Link to="/" style={{ marginRight: "20px", fontFamily: 'Orbitron' }}>Home</Link>
+                        <Link to="/showcase" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>Showcase</Link>
+                        <Link to="/about" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>About us</Link>
+                        < ButtonLogout />
+                    </div>
+        
+                    <NavItems>
+                        <ToolTip title={<h2 style={{ fontFamily: 'Orbitron' }}>Click to Login or Register</h2>} placement="left" arrow>
+                            <Link to="/login"><VscGear style={{ stroke: "url(#blue-gradient)" }} /></Link> 
+                        </ToolTip>
+                    </NavItems>
+                </NavBarWrapper>
+            </>
+            )
+    }
 }
