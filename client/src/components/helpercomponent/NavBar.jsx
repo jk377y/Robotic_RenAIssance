8079424ac067215efdef906308d9325bf0092437
Sofia92 <sofiasosaperez@outlook.com>
2023-04-13 21:39:55 -0500

    moved link home to the left

--- client/src/components/helpercomponent/NavBar.jsx
+++ client/src/components/helpercomponent/NavBar.jsx
@@ -11,6 +11,11 @@ export const NavBar = () => {
         return (
             <>
             <NavBarWrapper>
+                            
+            <div style={{ display: "flex", alignItems: "center", marginRight: "auto" }}>
+                    <Link to="/" style={{ marginRight: "20px", fontFamily: 'Orbitron' }}>Home</Link>
+                </div>
+            
                 <div style={{ display: "flex", alignItems: "center", marginRight: "auto" }}>
                     <Link to="/" style={{ marginRight: "20px", fontFamily: 'Orbitron' }}>Home</Link>
                     <Link to="/showcase" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>Showcase</Link>
@@ -28,8 +33,9 @@ export const NavBar = () => {
     } else {
         return (
             <><NavBarWrapper>
+<div style={{ display: "flex", alignItems: "center", marginRight: "auto" }}><Link to="/" style={{ marginRight: "20px", fontFamily: 'Orbitron' }}>Home</Link>
+</div>
             <div style={{ display: "flex", alignItems: "center", justifyContent: "flex-end" }}>
-              <Link to="/" style={{ marginRight: "20px", fontFamily: 'Orbitron' }}>Home</Link>
               <Link to="/showcase" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>Showcase</Link>
               <Link to="/about" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>About us</Link>
               {Auth.loggedIn() && <ButtonLogout />}
@@ -45,3 +51,56 @@ export const NavBar = () => {
     }
     
 }
+
+//original code:
+// import { NavBarWrapper, NavItems } from "../styles/helpers/Navbar.styled";
+// import { VscGear } from "react-icons/vsc";
+// import ToolTip from '@mui/material/Tooltip';
+// import { Link } from "react-router-dom";
+
+// import { ButtonLogout } from "./ButtonLogout";
+// import Auth from '../../utils/auth'
+
+// export const NavBar = () => {
+//     if (!Auth.loggedIn()) {
+//         return (
+//             <>
+//             <NavBarWrapper>
+//                 <div style={{ display: "flex", alignItems: "center", marginRight: "auto" }}>
+//                     <Link to="/" style={{ marginRight: "20px", fontFamily: 'Orbitron' }}>Home</Link>
+//                     <Link to="/showcase" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>Showcase</Link>
+//                     <Link to="/about" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>About us</Link>
+//                 </div>
+            
+    
+//                 <NavItems>
+//                     <ToolTip title={<h2 style={{ fontFamily: 'Orbitron' }}>Click to Login or Register</h2>} placement="left" arrow>
+//                         <Link to="/login"><VscGear style={{ stroke: "url(#blue-gradient)" }} /></Link> 
+//                     </ToolTip>
+//                 </NavItems>
+//             </NavBarWrapper>
+//         </>
+//         )
+//     } else {
+//         return (
+//             <>
+//             <NavBarWrapper>
+//                 <div style={{ display: "flex", alignItems: "center", marginRight: "auto" }}>
+//                     <Link to="/" style={{ marginRight: "20px", fontFamily: 'Orbitron' }}>Home</Link>
+//                     <Link to="/showcase" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>Showcase</Link>
+//                     <Link to="/about" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>About us</Link>
+//                     < ButtonLogout />
+//                 </div>
+            
+    
+//                 <NavItems>
+//                     <ToolTip title={<h2 style={{ fontFamily: 'Orbitron' }}>Click to Login or Register</h2>} placement="left" arrow>
+//                         <Link to="/login"><VscGear style={{ stroke: "url(#blue-gradient)" }} /></Link> 
+//                     </ToolTip>
+//                 </NavItems>
+//             </NavBarWrapper>
+//         </>
+//         )
+//     }
+    
+// }
