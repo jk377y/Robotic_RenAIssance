7db4dd86adc05c0748f30a9b28f65e3ca3dbfe0b
Sofia92 <sofiasosaperez@outlook.com>
2023-04-13 21:54:02 -0500

    fixed link error in signed in navbar

--- client/src/components/helpercomponent/NavBar.jsx
+++ client/src/components/helpercomponent/NavBar.jsx
@@ -14,12 +14,12 @@ export const NavBar = () => {
                             
             <div style={{ display: "flex", alignItems: "center", marginRight: "auto" }}>
                     <Link to="/" style={{ marginRight: "20px", fontFamily: 'Orbitron' }}>Home</Link>
-                </div>
+            </div>
             
-                <div style={{ display: "flex", alignItems: "center", marginRight: "auto" }}>
-                    <Link to="/" style={{ marginRight: "20px", fontFamily: 'Orbitron' }}>Home</Link>
-                    <Link to="/showcase" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>Showcase</Link>
-                    <Link to="/about" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>About us</Link>
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "flex-end" }}>
+    
+    <Link to="/showcase" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>Showcase</Link>
+    <Link to="/about" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>About us</Link>
                 </div>
             
                 <NavItems>
@@ -33,26 +33,30 @@ export const NavBar = () => {
     } else {
         return (
             <><NavBarWrapper>
-<div style={{ display: "flex", alignItems: "center", marginRight: "auto" }}><Link to="/" style={{ marginRight: "20px", fontFamily: 'Orbitron' }}>Home</Link>
-</div>
-            <div style={{ display: "flex", alignItems: "center", justifyContent: "flex-end" }}>
-              <Link to="/showcase" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>Showcase</Link>
-              <Link to="/about" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>About us</Link>
-              {Auth.loggedIn() && <ButtonLogout />}
+                              
+            <div style={{ display: "flex", alignItems: "center", marginRight: "auto" }}>
+                    <Link to="/" style={{ marginRight: "20px", fontFamily: 'Orbitron' }}>Home</Link>
             </div>
-            <NavItems>
-              <ToolTip title={<h2 style={{ fontFamily: 'Orbitron' }}>Click to Login or Register</h2>} placement="left" arrow>
-                <Link to="/login"><VscGear style={{ stroke: "url(#blue-gradient)" }} /></Link> 
-              </ToolTip>
-            </NavItems>
-          </NavBarWrapper>
+  <div style={{ display: "flex", alignItems: "center", justifyContent: "flex-end" }}>
+    
+    <Link to="/showcase" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>Showcase</Link>
+    <Link to="/about" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>About us</Link>
+    {Auth.loggedIn() && <ButtonLogout />}
+  </div>
+  <NavItems>
+    <ToolTip title={<h2 style={{ fontFamily: 'Orbitron' }}>Click to Login or Register</h2>} placement="left" arrow>
+      <Link to="/login"><VscGear style={{ stroke: "url(#blue-gradient)" }} /></Link> 
+    </ToolTip>
+  </NavItems>
+</NavBarWrapper>
+
         </>
         )
     }
     
 }
 
-//original code:
+
 // import { NavBarWrapper, NavItems } from "../styles/helpers/Navbar.styled";
 // import { VscGear } from "react-icons/vsc";
 // import ToolTip from '@mui/material/Tooltip';
