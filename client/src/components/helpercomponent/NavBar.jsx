e76d732f360014e5870fcb5fd955d6dc4b2f85d3
Sofia92 <sofiasosaperez@outlook.com>
2023-04-13 21:24:50 -0500

    moved links to the right of the navbar

--- client/src/components/helpercomponent/NavBar.jsx
+++ client/src/components/helpercomponent/NavBar.jsx
@@ -17,7 +17,6 @@ export const NavBar = () => {
                     <Link to="/about" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>About us</Link>
                 </div>
             
-    
                 <NavItems>
                     <ToolTip title={<h2 style={{ fontFamily: 'Orbitron' }}>Click to Login or Register</h2>} placement="left" arrow>
                         <Link to="/login"><VscGear style={{ stroke: "url(#blue-gradient)" }} /></Link> 
@@ -28,22 +27,19 @@ export const NavBar = () => {
         )
     } else {
         return (
-            <>
-            <NavBarWrapper>
-                <div style={{ display: "flex", alignItems: "center", marginRight: "auto" }}>
-                    <Link to="/" style={{ marginRight: "20px", fontFamily: 'Orbitron' }}>Home</Link>
-                    <Link to="/showcase" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>Showcase</Link>
-                    <Link to="/about" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>About us</Link>
-                    < ButtonLogout />
-                </div>
-            
-    
-                <NavItems>
-                    <ToolTip title={<h2 style={{ fontFamily: 'Orbitron' }}>Click to Login or Register</h2>} placement="left" arrow>
-                        <Link to="/login"><VscGear style={{ stroke: "url(#blue-gradient)" }} /></Link> 
-                    </ToolTip>
-                </NavItems>
-            </NavBarWrapper>
+            <><NavBarWrapper>
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "flex-end" }}>
+              <Link to="/" style={{ marginRight: "20px", fontFamily: 'Orbitron' }}>Home</Link>
+              <Link to="/showcase" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>Showcase</Link>
+              <Link to="/about" style={{ fontFamily: 'Orbitron', marginRight: "20px" }}>About us</Link>
+              {Auth.loggedIn() && <ButtonLogout />}
+            </div>
+            <NavItems>
+              <ToolTip title={<h2 style={{ fontFamily: 'Orbitron' }}>Click to Login or Register</h2>} placement="left" arrow>
+                <Link to="/login"><VscGear style={{ stroke: "url(#blue-gradient)" }} /></Link> 
+              </ToolTip>
+            </NavItems>
+          </NavBarWrapper>
         </>
         )
     }
