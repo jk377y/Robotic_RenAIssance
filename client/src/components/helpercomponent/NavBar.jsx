0252075e512d7f8e9f6f4f457b1193b41047b366
Sofia92 <sofiasosaperez@outlook.com>
2023-04-16 01:16:31 -0500

    cleaned up code/ file complete

--- client/src/components/helpercomponent/NavBar.jsx
+++ client/src/components/helpercomponent/NavBar.jsx
@@ -1,80 +1,9 @@
-//old code
-// import { NavBarWrapper, NavItems } from "../styles/helpers/Navbar.styled";
-// import { VscGear } from "react-icons/vsc";
-// import ToolTip from '@mui/material/Tooltip';
-// import { Link } from "react-router-dom";
-
-// import { ButtonLogout } from "./ButtonLogout";
-// import Auth from '../../utils/auth'
-
-// const linkStyle = {
-//     fontFamily: 'Orbitron',
-//     marginRight: '20px',
-//     borderRadius: '50px',
-//     padding: '10px 10px',
-//     backgroundColor: '#fff',
-//     color: '#000',
-//     textDecoration: 'none',
-//     transition: 'background-color 0.3s ease-in-out',
-//     boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)'
-// }
-
-
-// export const NavBar = () => {
-//     if (!Auth.loggedIn()) {
-//         return (
-//             <>
-//                 <NavBarWrapper>
-//                     <div style={{ display: "flex", alignItems: "center", marginRight: "auto" }}>
-//                         <Link to="/" style={{ ...linkStyle, marginRight: "20px" }}>Home</Link>
-//                     </div>
-//                     <div style={{ display: "flex", alignItems: "center", justifyContent: "flex-end" }}>
-//                         <Link to="/showcase" style={linkStyle}>Showcase</Link>
-//                         <Link to="/about" style={linkStyle}>About us</Link>
-//                     </div>
-//                     <NavItems>
-//                         <ToolTip title={<h2 style={{ fontFamily: 'Orbitron' }}>Click to Login or Register</h2>} placement="left" arrow>
-//                             <Link to="/login"><VscGear /></Link>
-//                         </ToolTip>
-//                     </NavItems>
-//                 </NavBarWrapper>
-//             </>
-//         )
-//     } else {
-//         return (
-//             <>
-//                 <NavBarWrapper>
-//                     <div style={{ display: "flex", alignItems: "center", marginRight: "auto" }}>
-//                         <Link to="/" style={{ ...linkStyle, marginRight: "20px" }}>Home</Link>
-//                     </div>
-//                     <div style={{ display: "flex", alignItems: "center", justifyContent: "flex-end" }}>
-//                         <Link to="/showcase" style={linkStyle}>Showcase</Link>
-//                         <Link to="/about" style={linkStyle}>About us</Link>
-//                         {Auth.loggedIn() && <ButtonLogout />}
-//                     </div>
-//                     <NavItems>
-//                         <ToolTip title={<h2 style={{ fontFamily: 'Orbitron' }}>Click to Login or Register</h2>} placement="left" arrow>
-//                             <Link to="/login"><VscGear /></Link>
-//                         </ToolTip>
-//                     </NavItems>
-//                 </NavBarWrapper>
-//             </>
-//         )
-//     }
-// }
-
-//new code for proper logout/login button functionality
-
-
-
-
 
 import { NavBarWrapper, NavItems } from "../styles/helpers/Navbar.styled";
 import { VscGear } from "react-icons/vsc";
 import ToolTip from '@mui/material/Tooltip';
 import { Link } from "react-router-dom";
 
-import { ButtonLogout } from "./ButtonLogout";
 import Auth from '../../utils/auth'
 
 const linkStyle = {
@@ -101,31 +30,24 @@ export const NavBar = () => {
                 <div style={{ display: "flex", alignItems: "center", justifyContent: "flex-end" }}>
                     <Link to="/showcase" style={linkStyle}>Showcase</Link>
                     <Link to="/about" style={linkStyle}>About us</Link>
-                    
-                    {/* <NavItems>
-                        {isLoggedIn && <ButtonLogout style={linkStyle} />}
-                    </NavItems> */}
-                    
+
+
                     {isLoggedIn && (
-                    // <ButtonLogout>
-                    //     <ToolTip title={<h2 style={{ fontFamily: 'Orbitron' }}>Logout</h2>} placement="left" arrow>
-                    //     <Link to="/"><VscGear /></Link>
-                    //     </ToolTip> 
-                    // </ButtonLogout>
-                    <NavItems>
-                        <ToolTip title={<h2 style={{ fontFamily: 'Orbitron' }}>Logout</h2>} placement="left" arrow>
-                        <Link to="/"><VscGear onClick={() => Auth.logout()}/></Link> 
-                        </ToolTip>                     
+
+                        <NavItems>
+                            <ToolTip title={<h2 style={{ fontFamily: 'Orbitron' }}>Logout</h2>} placement="left" arrow>
+                                <Link to="/"><VscGear onClick={() => Auth.logout()} /></Link>
+                            </ToolTip>
                         </NavItems>
-                )}
+                    )}
                 </div>
                 {!isLoggedIn && (
                     <NavItems>
 
                         <ToolTip title={<h2 style={{ fontFamily: 'Orbitron' }}>Click to Login or Register</h2>} placement="left" arrow>
                             <Link to="/login"><VscGear /></Link>
-                        </ToolTip>                     
-                        </NavItems>
+                        </ToolTip>
+                    </NavItems>
                 )}
             </NavBarWrapper>
         </>
