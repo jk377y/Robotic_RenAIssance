7d53a9f9533427ff19b612fa3c61caea657c656b
Sofia92 <sofiasosaperez@outlook.com>
2023-04-16 02:18:52 -0500

    added shadow to links

--- client/src/components/helpercomponent/NavBar.jsx
+++ client/src/components/helpercomponent/NavBar.jsx
@@ -1,4 +1,61 @@
 
+// import { NavBarWrapper, NavItems } from "../styles/helpers/Navbar.styled";
+// import { VscGear } from "react-icons/vsc";
+// import ToolTip from '@mui/material/Tooltip';
+// import { Link } from "react-router-dom";
+
+// import Auth from '../../utils/auth'
+
+// const linkStyle = {
+//     fontFamily: 'Orbitron',
+//     marginRight: '20px',
+//     borderRadius: '50px',
+//     padding: '10px 10px',
+//     backgroundColor: '#fff',
+//     color: '#000',
+//     textDecoration: 'none',
+//     transition: 'background-color 0.3s ease-in-out',
+//     boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)'
+// }
+
+
+// export const NavBar = () => {
+//     const isLoggedIn = Auth.loggedIn();
+
+//     return (
+//         <>
+//             <NavBarWrapper>
+//                 <div style={{ display: "flex", alignItems: "center", marginRight: "auto" }}>
+//                     <Link to="/" style={{ ...linkStyle, marginRight: "20px" }}>Home</Link>
+//                 </div>
+//                 <div style={{ display: "flex", alignItems: "center", justifyContent: "flex-end" }}>
+//                     <Link to="/showcase" style={linkStyle}>Showcase</Link>
+//                     <Link to="/about" style={linkStyle}>About us</Link>
+
+
+//                     {isLoggedIn && (
+
+//                         <NavItems>
+//                             <ToolTip title={<h2 style={{ fontFamily: 'Orbitron' }}>Logout</h2>} placement="left" arrow>
+//                                 <Link to="/"><VscGear onClick={() => Auth.logout()} /></Link>
+//                             </ToolTip>
+//                         </NavItems>
+//                     )}
+//                 </div>
+//                 {!isLoggedIn && (
+//                     <NavItems>
+
+//                         <ToolTip title={<h2 style={{ fontFamily: 'Orbitron' }}>Click to Login or Register</h2>} placement="left" arrow>
+//                             <Link to="/login"><VscGear /></Link>
+//                         </ToolTip>
+//                     </NavItems>
+//                 )}
+//             </NavBarWrapper>
+//         </>
+//     )
+// }
+
+
 import { NavBarWrapper, NavItems } from "../styles/helpers/Navbar.styled";
 import { VscGear } from "react-icons/vsc";
 import ToolTip from '@mui/material/Tooltip';
@@ -14,8 +71,14 @@ const linkStyle = {
     backgroundColor: '#fff',
     color: '#000',
     textDecoration: 'none',
-    transition: 'background-color 0.3s ease-in-out',
-    boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)'
+    transition: 'background-color 0.3s ease-in-out, box-shadow 0.3s ease-in-out',
+    boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
+    cursor: 'pointer',
+};
+
+const pinkLinkStyle = {
+    color: 'pink',
+    textDecoration: 'none',
 }
 
 export const NavBar = () => {
@@ -25,15 +88,52 @@ export const NavBar = () => {
         <>
             <NavBarWrapper>
                 <div style={{ display: "flex", alignItems: "center", marginRight: "auto" }}>
-                    <Link to="/" style={{ ...linkStyle, marginRight: "20px" }}>Home</Link>
+                    <Link
+                        to="/"
+                        style={{
+                            ...linkStyle, marginRight: "20px",
+                            boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
+                            transition: "box-shadow 0.3s ease-in-out",
+                        }}
+                        onMouseOver={(e) => (e.target.style.boxShadow = "0 0 10px rgba(0, 191, 255, 0.5)")}
+                        onMouseOut={(e) => (e.target.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.5)")}
+                    >
+                        Home 
+                    </Link>
+
                 </div>
                 <div style={{ display: "flex", alignItems: "center", justifyContent: "flex-end" }}>
-                    <Link to="/showcase" style={linkStyle}>Showcase</Link>
-                    <Link to="/about" style={linkStyle}>About us</Link>
 
+                    <Link
+                        to="/showcase"
+                        style={{
+                            ...linkStyle,
+                            boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
+                            transition: "box-shadow 0.3s ease-in-out",
+                        }}
+                        onMouseOver={(e) => (e.target.style.boxShadow = "0 0 10px rgba(0, 191, 255, 0.5)")}
+                        onMouseOut={(e) => (e.target.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.5)")}
+                    >
+                        Showcase
+                    </Link>
+
+
+
+
+                    <Link
+                        to="/about"
+                        style={{
+                            ...linkStyle, marginRight: "20px",
+                            boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
+                            transition: "box-shadow 0.3s ease-in-out",
+                        }}
+                        onMouseOver={(e) => (e.target.style.boxShadow = "0 0 10px rgba(0, 191, 255, 0.5)")}
+                        onMouseOut={(e) => (e.target.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.5)")}
+                    >
+                        About Us 
+                    </Link>
 
                     {isLoggedIn && (
-
                         <NavItems>
                             <ToolTip title={<h2 style={{ fontFamily: 'Orbitron' }}>Logout</h2>} placement="left" arrow>
                                 <Link to="/"><VscGear onClick={() => Auth.logout()} /></Link>
@@ -43,7 +143,6 @@ export const NavBar = () => {
                 </div>
                 {!isLoggedIn && (
                     <NavItems>
-
                         <ToolTip title={<h2 style={{ fontFamily: 'Orbitron' }}>Click to Login or Register</h2>} placement="left" arrow>
                             <Link to="/login"><VscGear /></Link>
                         </ToolTip>
@@ -53,3 +152,6 @@ export const NavBar = () => {
         </>
     )
 }
+
+
+
